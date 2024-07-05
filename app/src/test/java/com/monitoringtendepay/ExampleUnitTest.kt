ee0fbbe9a5bedc1e7e81a6e0b029580a3247716e
mutukuian <mutukui940@gmail.com>
2024-07-05 11:47:01 +0300

    add klint for static code analysis

--- app/src/test/java/com/monitoringtendepay/ExampleUnitTest.kt
+++ app/src/test/java/com/monitoringtendepay/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.monitoringtendepay
 
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
