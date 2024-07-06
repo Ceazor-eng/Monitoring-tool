4e8617ae6f7a29c9ceb517469bf3ed554e59e5cb
mutukuian <mutukui940@gmail.com>
2024-07-06 10:36:25 +0300

    fix dependencies structure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-    id("kotlinx-serialization")
+    id("org.jetbrains.kotlin.plugin.serialization")
     id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
 }
@@ -10,18 +10,13 @@ android {
     namespace = "com.monitoringtendepay"
     compileSdk = 34
 
-
-
     defaultConfig {
         applicationId = "com.monitoringtendepay"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
-
     }
 
     buildTypes {
@@ -33,29 +28,40 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = "17"
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
+    // Hilt dependencies
+    implementation(libs.dagger.hilt)
+    kapt(libs.kapt)
+
+    // Kotlin Serialization dependencies
+    implementation(libs.serialization.json)
+
+    // Other dependencies
+    implementation(libs.bundles.networking)
+
+    // Test dependencies
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-
-    implementation(libs.bundles.networking)
-    implementation(libs.bundles.dagger)
 }
+
 // Allow references to generated code
 kapt {
     correctErrorTypes = true
