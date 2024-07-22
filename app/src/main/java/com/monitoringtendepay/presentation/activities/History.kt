15917fe4c1964f832c13f74cae41b416e78c9946
Ceazor-eng <timothyomondi02@gmail.com>
2024-07-22 21:20:47 +0300

    History UI complete

---
+++ app/src/main/java/com/monitoringtendepay/presentation/activities/History.kt
@@ -0,0 +1,23 @@
+package com.monitoringtendepay.presentation.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.monitoringtendepay.R
+
+public class History : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.profile)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) {
+                v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- app/src/main/java/com/monitoringtendepay/presentation/activities/Profile.kt
+++ app/src/main/java/com/monitoringtendepay/presentation/activities/Profile.kt
@@ -19,6 +19,6 @@ class Profile : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        return inflater.inflate(R.layout.profile, container, false)
     }
 }
