96d364462b9f402b78d00150d2517069d636e313
mutukuian <mutukui940@gmail.com>
2024-07-30 15:40:50 +0300

    implement authViewmodel test for login functionality

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     // Hilt dependencies
     implementation(libs.dagger.hilt)
     implementation(libs.androidx.swiperefreshlayout)
+    // testImplementation(libs.junit.junit)
     kapt(libs.kapt)
 
     // Kotlin Serialization dependencies
@@ -78,6 +79,24 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    testImplementation("org.mockito:mockito-core:5.7.0")
+    androidTestImplementation("org.mockito:mockito-core:5.7.0")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
+    androidTestImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
+
+    // Use mockito-android for Android instrumentation tests
+    androidTestImplementation("org.mockito:mockito-android:4.4.0")
+    androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
+
+    testImplementation("io.mockk:mockk:1.12.0")
+    androidTestImplementation("io.mockk:mockk-android:1.12.0")
+
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    testImplementation("org.robolectric:robolectric:4.9")
+
+    // truth
+    testImplementation("com.google.truth:truth:1.0.1")
 }
 
 // Allow references to generated code
